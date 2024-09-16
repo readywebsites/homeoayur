0f7ad2dddd9eec45adb9ecead21ae6532f389bfe
readywebsites <biz499websites@gmail.com>
2024-09-16 11:55:35 +0530

    removed linkedin from settings.py

--- homeoayurcart/settings.py
+++ homeoayurcart/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
      'allauth.socialaccount',
      'allauth.socialaccount.providers.google',  
      'allauth.socialaccount.providers.facebook',  
-    'allauth.socialaccount.providers.linkedin',  
+    # 'allauth.socialaccount.providers.linkedin',  
 
 ]
 
