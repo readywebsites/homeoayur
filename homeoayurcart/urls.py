ecaad23877da942600354eebd0badeb6f5daae36
rajilpatel <rajilpatel001@gmail.com>
2024-12-21 11:22:11 +0530

    admin name and icon remove

--- homeoayurcart/urls.py
+++ homeoayurcart/urls.py
@@ -50,3 +50,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Sodam Admin"
+admin.site.site_title = "Sodam Admin Portal"
+admin.site.index_title = "Welcome to Sodam Admin Portal"
