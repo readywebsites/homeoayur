3c4c3d77bc6e48393c9474ad1b114e724e9ee5f6
rajilpatel <rajilpatel001@gmail.com>
2025-02-10 12:08:32 +0530

    sqlite try for phone number

--- homeoayurcart/settings.py
+++ homeoayurcart/settings.py
@@ -89,15 +89,8 @@ WSGI_APPLICATION = 'homeoayurcart.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+    'default': {        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'homeoayurdb',
-        'USER': 'homeoayuruser',
-        'PASSWORD': '123homeoayuraA@A',
-        'HOST': 'localhost',
-        'PORT': '',
     }
 }
 
