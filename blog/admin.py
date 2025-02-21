ad7c06db1604bb832b861ddf489421d2934a37b8
rajilpatel <rajilpatel001@gmail.com>
2025-02-21 14:29:26 +0530

    added newslettersignup in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Blog_Post,Team, Category, Hashtags
+from .models import Blog_Post,Team, Category, Hashtags, NewsletterSignup
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Blog_Post)
 admin.site.register(Team)
 admin.site.register(Category)
 admin.site.register(Hashtags)
+admin.site.register(NewsletterSignup)
