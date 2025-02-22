a1d86799c99d729a27c3d6e8660f8ccf8ea30fc8
rajilpatel <rajilpatel001@gmail.com>
2025-02-22 12:03:32 +0530

    '/phone-login/'  logn url in settings

--- homeoayurcart/settings.py
+++ homeoayurcart/settings.py
@@ -148,7 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
-LOGIN_URL = '/accounts/login/'
+LOGIN_URL = '/phone-login/' 
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
